0496e9e0557f661cd799212c2d54cfb0faa982f7
Acexy <thankjava@foxmail.com>
2017-02-10 10:13:40 +0800

    新增远程命令指定服务器

--- utils/remoteCommandExec.js
+++ utils/remoteCommandExec.js
@@ -46,3 +46,45 @@ module.exports.exe = function (cmd, cb) {
         }
     });
 };
+
+module.exports.exeArgv = function (cmd, cb, server) {
+    var conn = new Client();
+    conn.on('ready', function () {
+        conn.exec(cmd, function (err, stream) {
+            if (err) {
+                cb(err, null, null);
+                return;
+            }
+            var sData, eData = null;
+            stream.on('close', function (code, signal) {
+                // console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+                conn.end();
+                cb(null, sData, eData);
+            }).on('data', function (data) {
+                sData = data.toString();
+            }).stderr.on('data', function (data) {
+                eData = data.toString();
+            });
+        });
+    }).on('error', function (err) {
+        cb(err, null, null);
+    }).connect({
+        host: server.host,
+        port: server.port,
+        username: server.user,
+        password: server.pass,
+        algorithms: {
+            hmac: [
+                'hmac-sha2-256',
+                'hmac-sha2-512',
+                'hmac-sha1',
+                'hmac-md5',
+                'hmac-sha2-256-96',
+                'hmac-sha2-512-96',
+                'hmac-ripemd160',
+                'hmac-sha1-96',
+                'hmac-md5-96'
+            ]
+        }
+    });
+};
